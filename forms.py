3106017eee4f0c33c178f957600ad4be723dc1ce
bbelderbos <bobbelderbos@gmail.com>
2017-08-16 03:04:25 +0200

    flake8 forms.py

--- forms.py
+++ forms.py
@@ -1,17 +1,20 @@
 import glob
 import os
 
-from wtforms import Form, BooleanField, StringField, SelectField, TextAreaField, validators
+from wtforms import Form, BooleanField, StringField
+from wtforms import SelectField, TextAreaField, validators
 
 pybites_logos = glob.glob(os.path.join('assets', 'pybites', '*png'))
 
-option_field = lambda img: os.path.splitext(os.path.basename(img))[0]
+
+def get_basename(img):
+    return os.path.splitext(os.path.basename(img))[0]
 
 
 class ImageForm(Form):
     image_url1 = SelectField(
         'PyBites Logo Theme',
-        choices=[(img, option_field(img)) for img in pybites_logos]
+        choices=[(img, get_basename(img)) for img in pybites_logos]
     )
     image_url2 = StringField('Second Image URL', [validators.DataRequired()])
     text = TextAreaField('Text for Banner', [validators.DataRequired()])
